0fefe79d3a947ed0fb67e87a45293f60096153ec
Shawn Zaleski <ek7121@wayne.edu>
2015-10-28 18:12:13 +0100

    add run/event/lumi information and many many more

--- MuonAnalyzer/python/wsuMuonAnalyzer_cfi.py
+++ MuonAnalyzer/python/wsuMuonAnalyzer_cfi.py
@@ -1,7 +1,9 @@
 import FWCore.ParameterSet.Config as cms
 
 muonAnalysis = cms.EDAnalyzer('MuonAnalyzer',
-    muonSrc = cms.InputTag("muons1Leg")
+    muonSrc = cms.InputTag("muons1Leg"),
+                              algoType = cms.int32(1), #Different values for track type can be found in the config file.
+                              debug = cms.int32(0) #debugging uses integer for verbosity of mode 0 = debug mode "off"
 #vector<reco::Muon>                    "lhcSTAMuons"               ""                "RECO"    
 #vector<reco::Muon>                    "muons"                     ""                "RECO"    
 #vector<reco::Muon>                    "muons1Leg"                 ""                "RECO"    
