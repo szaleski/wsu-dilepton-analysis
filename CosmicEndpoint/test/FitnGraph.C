6f6eb1b6618a471e3bb25394a578a5911facb6a1
Shawn Zaleski <ek7121@wayne.edu>
2015-11-09 12:39:28 +0100

    Fixed graph fitter

--- CosmicEndpoint/test/FitnGraph.C
+++ CosmicEndpoint/test/FitnGraph.C
@@ -5,7 +5,7 @@
   std::cout << "\nThe file is open" <<   file->IsOpen()  << std::endl;
 
     std::cout << "\nThe file is Zombie" <<   file->IsZombie() << std::endl;
-  TGraph *graph = (TGraph*)tunep_Chi2; //file->Get("tunep_Chi2");
+  TGraph *graph = (TGraph*)graph_tunep_Chi2; //file->Get("tunep_Chi2");
 
   std::cout << "\nThe graph to be read is " << graph << std::endl;
   TF1 *fitPoly = new TF1("fitPoly","pol6", -1,1);
@@ -21,4 +21,7 @@
   std::cout << "\nDrew graph" << std::endl;
   fitPoly->Draw("sames");
   std::cout << "\nDrew the Fit" << std::endl;
+
+  graph->SaveAs("testGraphChi2.jpg");
+  graph->SaveAs("testGraphChi2.pdf");
 }
