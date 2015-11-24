6fd7016c02f2f7281b25c40dd4974ae39199f994
Jared Sturdy <sturdy@cern.ch>
2015-11-24 16:10:34 +0100

    added asym/sym to the directory name, probably not critical as the output files have asym/sym in the name

--- CosmicEndpoint/test/macrotools.py
+++ CosmicEndpoint/test/macrotools.py
@@ -4,11 +4,11 @@ import os,socket
 import shutil
 
 # copied from John Hakala's bhmacros and modified for quick use here
-def callMacro(macroName, inputFile, outputFile):
-	macroCommand = "%s(\"%s\", \"%s\")" % (macroName, inputFile, outputFile)
-	subprocess.call(["root", "-l", "-q", macroCommand])
+def callMacro(macroName,inputFile,outputFile):
+	macroCommand = "%s(\"%s\", \"%s\")" % (macroName,inputFile,outputFile)
+	subprocess.call(["root", "-l", "-q",macroCommand])
 
-def splitJobsForBsub(inputFile, numberOfJobs, maxBias, minPt, nBiasBins, symasym):
+def splitJobsForBsub(inputFile,numberOfJobs,maxBias,minPt,nBiasBins,symasym):
 	samplesListsDir="samplesLists_data"
 	inputFilePath = samplesListsDir+"/"+inputFile
 	num_input_samples = sum(1 for line in open(inputFilePath))
@@ -21,10 +21,10 @@ def splitJobsForBsub(inputFile, numberOfJobs, maxBias, minPt, nBiasBins, symasym
 		f = open("%s/splitLists_b%.2f_pt%2.0f_n%d/split_%i_%s_%s"%(samplesListsDir,
 									 1000*maxBias,
 									 minPt,nBiasBins,
-									 fid, symasym,
-									 inputFile), 'w')
+									 fid,symasym,
+									 inputFile),'w')
 		for i,line in enumerate(inputSamplesList):
-			#print "%i : \n      %s" % (i, line)
+			#print "%i : \n      %s" % (i,line)
 			f.write(line)
 			lineswritten+=1
 			if lineswritten == chunksize:# and fid < numberOfJobs:
@@ -34,34 +34,34 @@ def splitJobsForBsub(inputFile, numberOfJobs, maxBias, minPt, nBiasBins, symasym
 				f = open("%s/splitLists_b%.2f_pt%2.0f_n%d/split_%i_%s_%s"%(samplesListsDir,
 											 1000*maxBias,
 											 minPt,nBiasBins,
-											 fid, symasym,
-											 inputFile), 'w')
+											 fid,symasym,
+											 inputFile),'w')
 		f.close()
 		return fid
 
-def bSubSplitJobs(pyScriptName, outputFile, inputFile, proxyPath, numberOfJobs,
-		  maxBias, minPt, nBiasBins, symmetric, debug):
+def bSubSplitJobs(pyScriptName,outputFile,inputFile,proxyPath,numberOfJobs,
+		  maxBias,minPt,nBiasBins,symmetric,debug):
 	symasym = "asym"
 	if symmetric:
 		symasym = "sym"
 
 	samplesListsDir="samplesLists_data"
 
-	if not os.path.exists("/tmp/%s/output_b%.2f_pt%2.0f_n%d"%(os.getlogin(),1000*maxBias,minPt,nBiasBins)):
-		os.makedirs("/tmp/%s/output_b%.2f_pt%2.0f_n%d"%(  os.getlogin(),1000*maxBias,minPt,nBiasBins))
+	if not os.path.exists("/tmp/%s/output_b%.2f_pt%2.0f_n%d_%s"%(os.getlogin(),1000*maxBias,minPt,nBiasBins,symasym)):
+		os.makedirs("/tmp/%s/output_b%.2f_pt%2.0f_n%d_%s"%(  os.getlogin(),1000*maxBias,minPt,nBiasBins,symasym))
 
 	rootScriptDir = "bsubs_b%.2f_pt%2.0f_n%d/roots"%(1000*maxBias,minPt,nBiasBins)
 	if not os.path.exists(rootScriptDir):
 		os.makedirs(rootScriptDir)
 
-	clearSplitLists(maxBias, minPt, nBiasBins, symasym)
-	clearBsubShellScripts(maxBias, minPt, nBiasBins, symasym)
-	nJobs = splitJobsForBsub(inputFile, numberOfJobs, maxBias, minPt, nBiasBins, symasym)
+	clearSplitLists(maxBias,minPt,nBiasBins,symasym)
+	clearBsubShellScripts(maxBias,minPt,nBiasBins,symasym)
+	nJobs = splitJobsForBsub(inputFile,numberOfJobs,maxBias,minPt,nBiasBins,symasym)
 	print "Prepared %i jobs ready to be submitted to bsub." % nJobs
-	for i in range (1, nJobs+1):
-		splitListFile="split_%i_%s_%s" % (i, symasym, inputFile)
-		rootScriptName = "root-%s-b%.2f_pt%2.0f_n%d_%s_%d.C"%(pyScriptName, 1000*maxBias,minPt,nBiasBins, symasym, i)
-		f = open("%s/%s"%(rootScriptDir,rootScriptName), "w")
+	for i in range (1,nJobs+1):
+		splitListFile="split_%i_%s_%s" % (i,symasym,inputFile)
+		rootScriptName = "root-%s-b%.2f_pt%2.0f_n%d_%s_%d.C"%(pyScriptName,1000*maxBias,minPt,nBiasBins,symasym,i)
+		f = open("%s/%s"%(rootScriptDir,rootScriptName),"w")
 		f.write("{\n")
 		#f.write("  gROOT->ProcessLine(\" .L %s/Plot.so\");\n"%(os.getcwd()))
 		inputFileList = samplesListsDir + "/splitLists_b%.2f_pt%2.0f_n%d/"%(1000*maxBias,minPt,nBiasBins) + splitListFile
@@ -72,25 +72,25 @@ def bSubSplitJobs(pyScriptName, outputFile, inputFile, proxyPath, numberOfJobs,
 										  symasym,outputFile,i,
 										  1,
 										  minPt,maxBias,nBiasBins,
-										  1000., symmetric))
+										  1000.,symmetric))
 		for tk in range(5):
 			f.write("  Plot(\"%s\",\"%s_%s_%d_\",%d, %f, %f, %d, %f, %d);\n"%(inputFileList,
 											  symasym,outputFile,i,
 											  tk+1,
 											  minPt,maxBias,nBiasBins,
-											  1000., symmetric))
+											  1000.,symmetric))
 		f.write("}\n")
 		# root -x -b -q  put this in the shell script
 		pyCommand = "%s"%(rootScriptName)
-		makeBsubShellScript(pyCommand, rootScriptDir, "%s/splitLists_b%.2f_pt%2.0f_n%d/%s"%(samplesListsDir,1000*maxBias,
+		makeBsubShellScript(pyCommand,rootScriptDir,"%s/splitLists_b%.2f_pt%2.0f_n%d/%s"%(samplesListsDir,1000*maxBias,
 												  minPt,nBiasBins,splitListFile),
-				    pyScriptName, i, proxyPath, maxBias, minPt, nBiasBins, symasym, debug)
+				    pyScriptName,i,proxyPath,maxBias,minPt,nBiasBins,symasym,debug)
 
-def makeBsubShellScript(pyCommand, rootScriptDir, splitListName, pyScriptName, index, proxyPath,
-			maxBias, minPt, nBiasBins, symasym, debug):
-	subfile = "%s/bsubs_b%.2f_pt%2.0f_n%d/bsub-%s-%s-%s.sh"%( os.getcwd(),1000*maxBias,minPt,nBiasBins,pyScriptName, symasym, index)
-	logfile = "%s/bsubs_b%.2f_pt%2.0f_n%d/bsub-%s-%s-%s.log"%(os.getcwd(),1000*maxBias,minPt,nBiasBins,pyScriptName, symasym, index)
-	f = open(subfile, "w")
+def makeBsubShellScript(pyCommand,rootScriptDir,splitListName,pyScriptName,index,proxyPath,
+			maxBias,minPt,nBiasBins,symasym,debug):
+	subfile = "%s/bsubs_b%.2f_pt%2.0f_n%d/bsub-%s-%s-%s.sh"%( os.getcwd(),1000*maxBias,minPt,nBiasBins,pyScriptName,symasym,index)
+	logfile = "%s/bsubs_b%.2f_pt%2.0f_n%d/bsub-%s-%s-%s.log"%(os.getcwd(),1000*maxBias,minPt,nBiasBins,pyScriptName,symasym,index)
+	f = open(subfile,"w")
 	f.write("""#!/bin/bash
 export VO_CMS_SW_DIR=/cvmfs/cms.cern.ch
 source $VO_CMS_SW_DIR/cmsset_default.sh
@@ -103,7 +103,7 @@ klist
 echo "hostname is $HOSTNAME"
 export JOBDIR=${PWD}
 echo "batch job directory is ${JOBDIR}"
-export OUTPUTDIR=${JOBDIR}/output_b%.2f_pt%2.0f_n%d
+export OUTPUTDIR=${JOBDIR}/output_b%.2f_pt%2.0f_n%d_%s
 echo "output directory is ${OUTPUTDIR}"
 mkdir ${OUTPUTDIR}
 ls -tar
@@ -121,7 +121,7 @@ echo "rsync \\"ssh -T -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/nul
 rsync -e "ssh -T -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" -aAXch --progress ${OUTPUTDIR} %s:/tmp/${USER}/
 """%(proxyPath,
      #logfile,logfile,
-     1000*maxBias,minPt,nBiasBins,
+     1000*maxBias,minPt,nBiasBins,symasym,
      #logfile,logfile,
      os.getcwd(),
      rootScriptDir,pyCommand,#logfile,
@@ -129,7 +129,7 @@ rsync -e "ssh -T -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" -a
      socket.gethostname(),
      socket.gethostname()))
 	f.close()
-	os.chmod(subfile, 0777)
+	os.chmod(subfile,0777)
 	# cmd = "bsub -q test -W 0:20 %s/%s"%(subfile) #submit to the test queue if debug (only a single job though
 	cmd = "bsub -q 8nh -W 240 %s"%(subfile)
 	#cmd = "bsub -q 1nh -W 0:30 %s"%(subfile)
@@ -137,7 +137,7 @@ rsync -e "ssh -T -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" -a
 	if not debug:
 		os.system(cmd)
 
-def clearSplitLists(maxBias, minPt, nBiasBins, symasym):
+def clearSplitLists(maxBias,minPt,nBiasBins,symasym):
 	samplesListsDir="samplesLists_data"
 	splitListsDir=samplesListsDir+"/splitLists_b%.2f_pt%2.0f_n%d/"%(1000*maxBias,minPt,nBiasBins)
 
@@ -145,27 +145,27 @@ def clearSplitLists(maxBias, minPt, nBiasBins, symasym):
 		os.makedirs(splitListsDir)
 
 	for the_file in os.listdir(splitListsDir):
-		file_path = os.path.join(splitListsDir, the_file)
+		file_path = os.path.join(splitListsDir,the_file)
 		if (file_path.find("_"+symasym+"_") > 0):
 			try:
 				if os.path.isfile(file_path):
 					os.unlink(file_path)
 				# elif os.path.isdir(file_path): shutil.rmtree(file_path)
-			except Exception, e:
+			except Exception,e:
 				print e
 
-def clearBsubShellScripts(maxBias, minPt, nBiasBins, symasym):
+def clearBsubShellScripts(maxBias,minPt,nBiasBins,symasym):
 	bSubScriptsDir="bsubs_b%.2f_pt%2.0f_n%d/"%(1000*maxBias,minPt,nBiasBins)
         #d = os.path.dirname(bSubScriptsDir)
         if not os.path.exists(bSubScriptsDir):
                 os.makedirs(bSubScriptsDir)
 
 	for the_file in os.listdir(bSubScriptsDir):
-		file_path = os.path.join(bSubScriptsDir, the_file)
+		file_path = os.path.join(bSubScriptsDir,the_file)
 		if (file_path.find("-"+symasym+"-") > 0):
 			try:
 				if os.path.isfile(file_path):
 					os.unlink(file_path)
 				# elif os.path.isdir(file_path): shutil.rmtree(file_path)
-			except Exception, e:
+			except Exception,e:
 				print e
