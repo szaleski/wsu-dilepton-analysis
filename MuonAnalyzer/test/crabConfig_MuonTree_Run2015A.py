cdb7d35cb4d607fa21f8799ca4e456556b3f6f1a
Jared Sturdy <sturdy@cern.ch>
2016-04-07 14:44:58 +0200

    updated crab MuonTree configs for latest JSON and full datasets

--- MuonAnalyzer/test/crabConfig_MuonTree_Run2015A.py
+++ MuonAnalyzer/test/crabConfig_MuonTree_Run2015A.py
@@ -3,7 +3,7 @@ from WMCore.Configuration import Configuration
 config = Configuration()
 
 config.section_('General')
-config.General.requestName = 'MuonEfficiencyTree_CRAFT15_Apr07'
+config.General.requestName = 'MuonEfficiencyTree_Run2015A_Apr07'
 config.General.workArea = 'crab_projects'
 config.General.transferOutputs = True
 config.General.transferLogs = False
@@ -13,15 +13,15 @@ config.JobType.pluginName = 'Analysis'
 config.JobType.psetName = 'wsuMuonTree_data.py'
 
 config.section_('Data')
-config.Data.inputDataset = '/Cosmics/Commissioning2015-CosmicSP-20Jan2016-v1/RAW-RECO'
+config.Data.inputDataset = '/Cosmics/Run2015A-CosmicSP-20Jan2016-v1/RAW-RECO'
 config.Data.inputDBS = 'global'
 config.Data.splitting = 'LumiBased'
-config.Data.unitsPerJob = 250
-config.Data.lumiMask = 'https://cmsdoc.cern.ch/~sturdy/Cosmics/JSON/21.03.2016/cosmics_CRAFT15_pix_strip_DT_RPC_complete.json'
+config.Data.unitsPerJob = 75
+config.Data.lumiMask = 'https://cmsdoc.cern.ch/~sturdy/Cosmics/JSON/21.03.2016/cosmics_Run2015_all_pix_strip_DT_RPC_complete.json'
 config.Data.allowNonValidInputDataset = True
 #config.Data.publication = True
 config.Data.outLFNDirBase = '/store/user/sturdy/MuonEfficiency'
-config.Data.outputDatasetTag = 'MuonEfficiencyTree_CRAFT15_Apr07'
+config.Data.outputDatasetTag = 'MuonEfficiencyTree_Run2015A_Apr07'
 
 config.section_('Site')
 config.Site.storageSite = 'T3_US_FNALLPC'
