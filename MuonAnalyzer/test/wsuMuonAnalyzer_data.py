94bc78d21f2e338faf47d1dfdb56f2a23c9328ec
Shawn Zaleski <ek7121@wayne.edu>
2016-04-08 19:06:37 +0200

    added dropDescendants to python config

--- MuonAnalyzer/test/wsuMuonAnalyzer_data.py
+++ MuonAnalyzer/test/wsuMuonAnalyzer_data.py
@@ -22,6 +22,7 @@ process.source.inputCommands = cms.untracked.vstring(
    "drop FEDRawDataCollection_rawDataCollector_*_*"
 )
 
+process.source.dropDescendantsOfDroppedBranches=cms.untracked.bool(False)
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
 process.load("WSUDiLeptons.MuonAnalyzer.wsuFakeL1SingleMuFilter_cfi")
