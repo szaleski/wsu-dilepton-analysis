29cc4d7a45fdf422638808130da16a93d5b013d3
Jared Sturdy <sturdy@cern.ch>
2016-04-11 14:17:20 +0200

    adding file lists and script to create them

---
+++ CosmicEndpoint/test/makeFileLists.sh
@@ -0,0 +1,30 @@
+
+lcg-ls --verbose -b -D srmv2 srm://cmseos.fnal.gov:8443/srm/v2/server?SFN=/eos/uscms/store/user/sturdy/MuonEfficiency/SPLooseMuCosmic_38T_p10/MuonEfficiencyTree_startup_peak_p10_Apr07/160407_161719/0000/   |fgrep root>& samplesLists_data/all_startup_peak_p10_trees_v5.txt
+lcg-ls --verbose -b -D srmv2 srm://cmseos.fnal.gov:8443/srm/v2/server?SFN=/eos/uscms/store/user/sturdy/MuonEfficiency/SPLooseMuCosmic_38T_p100/MuonEfficiencyTree_startup_peak_p100_Apr07/160407_161739/0000/ |fgrep root>& samplesLists_data/all_startup_peak_p100_trees_v5.txt
+lcg-ls --verbose -b -D srmv2 srm://cmseos.fnal.gov:8443/srm/v2/server?SFN=/eos/uscms/store/user/sturdy/MuonEfficiency/SPLooseMuCosmic_38T_p500/MuonEfficiencyTree_startup_peak_p500_Apr07/160407_161756/0000/ |fgrep root>& samplesLists_data/all_startup_peak_p500_trees_v5.txt
+
+lcg-ls --verbose -b -D srmv2 srm://cmseos.fnal.gov:8443/srm/v2/server?SFN=/eos/uscms/store/user/sturdy/MuonEfficiency/SPLooseMuCosmic_38T_p10/MuonEfficiencyTree_asym_deco_p10_Apr07/160407_161638/0000/   |fgrep root>& samplesLists_data/all_asymptotic_deco_p10_trees_v5.txt
+lcg-ls --verbose -b -D srmv2 srm://cmseos.fnal.gov:8443/srm/v2/server?SFN=/eos/uscms/store/user/sturdy/MuonEfficiency/SPLooseMuCosmic_38T_p100/MuonEfficiencyTree_asym_deco_p100_Apr07/160407_161652/0000/ |fgrep root>& samplesLists_data/all_asymptotic_deco_p100_trees_v5.txt
+lcg-ls --verbose -b -D srmv2 srm://cmseos.fnal.gov:8443/srm/v2/server?SFN=/eos/uscms/store/user/sturdy/MuonEfficiency/SPLooseMuCosmic_38T_p500/MuonEfficiencyTree_asym_deco_p500_Apr07/160407_161706/0000/ |fgrep root>& samplesLists_data/all_asymptotic_deco_p500_trees_v5.txt
+
+lcg-ls --verbose -b -D srmv2 srm://cmseos.fnal.gov:8443/srm/v2/server?SFN=/eos/uscms/store/user/sturdy/MuonEfficiency/Cosmics/MuonEfficiencyTree_CRAFT15_Apr07/160407_161538/0000/          |fgrep root>&  samplesLists_data/all_craft15_trees_v5.txt
+lcg-ls --verbose -b -D srmv2 srm://cmseos.fnal.gov:8443/srm/v2/server?SFN=/eos/uscms/store/user/sturdy/MuonEfficiency/Cosmics/MuonEfficiencyTree_CRAFT15_Apr07_recovery/160410_211038/0000  |fgrep root>>& samplesLists_data/all_craft15_trees_v5.txt
+lcg-ls --verbose -b -D srmv2 srm://cmseos.fnal.gov:8443/srm/v2/server?SFN=/eos/uscms/store/user/sturdy/MuonEfficiency/Cosmics/MuonEfficiencyTree_Run2015B_Apr07/160407_161250/0000/         |fgrep root>&  samplesLists_data/all_run2015b_trees_v5.txt
+lcg-ls --verbose -b -D srmv2 srm://cmseos.fnal.gov:8443/srm/v2/server?SFN=/eos/uscms/store/user/sturdy/MuonEfficiency/Cosmics/MuonEfficiencyTree_Run2015C_Apr07/160407_161552/0000/         |fgrep root>&  samplesLists_data/all_run2015c_trees_v5.txt
+lcg-ls --verbose -b -D srmv2 srm://cmseos.fnal.gov:8443/srm/v2/server?SFN=/eos/uscms/store/user/sturdy/MuonEfficiency/Cosmics/MuonEfficiencyTree_Run2015C_Apr07_recovery/160410_211052/0000 |fgrep root>>& samplesLists_data/all_run2015c_trees_v5.txt
+lcg-ls --verbose -b -D srmv2 srm://cmseos.fnal.gov:8443/srm/v2/server?SFN=/eos/uscms/store/user/sturdy/MuonEfficiency/Cosmics/MuonEfficiencyTree_Run2015D_Apr07/160407_161606/0000/         |fgrep root>&  samplesLists_data/all_run2015d_trees_v5.txt
+lcg-ls --verbose -b -D srmv2 srm://cmseos.fnal.gov:8443/srm/v2/server?SFN=/eos/uscms/store/user/sturdy/MuonEfficiency/Cosmics/MuonEfficiencyTree_Run2015D_Apr07_recovery/160410_211104/0000 |fgrep root>>& samplesLists_data/all_run2015d_trees_v5.txt
+lcg-ls --verbose -b -D srmv2 srm://cmseos.fnal.gov:8443/srm/v2/server?SFN=/eos/uscms/store/user/sturdy/MuonEfficiency/Cosmics/MuonEfficiencyTree_Run2015E_Apr07/160407_161622/0000/         |fgrep root>&  samplesLists_data/all_run2015e_trees_v5.txt
+
+lcg-ls --verbose -b -D srmv2 srm://cmseos.fnal.gov:8443/srm/v2/server?SFN=/eos/uscms/store/user/szaleski/Cosmics/MuonAnalysis_data_Apr_2016/160408_171105/0000 |fgrep root>& samplesLists_data/shawn_craft15_p100_v5.txt
+lcg-ls --verbose -b -D srmv2 srm://cmseos.fnal.gov:8443/srm/v2/server?SFN=/eos/uscms/store/user/szaleski/Cosmics/MuonAnalysis_data_Apr_2016/160407_192341/0000 |fgrep root>& samplesLists_data/shawn_run2015b_p100_v5.txt
+lcg-ls --verbose -b -D srmv2 srm://cmseos.fnal.gov:8443/srm/v2/server?SFN=/eos/uscms/store/user/szaleski/Cosmics/MuonAnalysis_data_Apr_2016/160407_192652/0000 |fgrep root>& samplesLists_data/shawn_run2015c_p100_v5.txt
+lcg-ls --verbose -b -D srmv2 srm://cmseos.fnal.gov:8443/srm/v2/server?SFN=/eos/uscms/store/user/szaleski/Cosmics/MuonAnalysis_data_Apr_2016/160407_192617/0000 |fgrep root>& samplesLists_data/shawn_run2015d_p100_v5.txt
+lcg-ls --verbose -b -D srmv2 srm://cmseos.fnal.gov:8443/srm/v2/server?SFN=/eos/uscms/store/user/szaleski/Cosmics/MuonAnalysis_data_Apr_2016/160407_192518/0000 |fgrep root>& samplesLists_data/shawn_run2015e_p100_v5.txt
+
+lcg-ls --verbose -b -D srmv2 srm://cmseos.fnal.gov:8443/srm/v2/server?SFN=/eos/uscms/store/user/szaleski/SPLooseMuCosmic_38T_p500/MuonAnalysis_data_Apr_2016/160407_192453/0000 |fgrep root>& samplesLists_data/shawn_asym_deco_p500_v5.txt
+lcg-ls --verbose -b -D srmv2 srm://cmseos.fnal.gov:8443/srm/v2/server?SFN=/eos/uscms/store/user/szaleski/SPLooseMuCosmic_38T_p500/MuonAnalysis_data_Apr_2016/160407_192241/0000 |fgrep root>& samplesLists_data/shawn_startup_peak_p500_v5.txt
+lcg-ls --verbose -b -D srmv2 srm://cmseos.fnal.gov:8443/srm/v2/server?SFN=/eos/uscms/store/user/szaleski/SPLooseMuCosmic_38T_p100/MuonAnalysis_data_Apr_2016/160407_192222/0000 |fgrep root>& samplesLists_data/shawn_asym_deco_p100_v5.txt
+lcg-ls --verbose -b -D srmv2 srm://cmseos.fnal.gov:8443/srm/v2/server?SFN=/eos/uscms/store/user/szaleski/SPLooseMuCosmic_38T_p100/MuonAnalysis_data_Apr_2016/160407_192309/0000 |fgrep root>& samplesLists_data/shawn_startup_peak_p100_v5.txt
+
+perl -pi -e 's/\/eos\/uscms\/store/\/store/g' samplesLists_data/*_v5.txt
